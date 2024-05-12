da7d43363f82f657e04ed74836d30b074a65974c
Christian Petersen <christian.n.petersen@gmail.com>
2024-05-12 17:26:07 +0200

    Added some buttons to change the window of the fft

--- pygameUpdate.py
+++ pygameUpdate.py
@@ -5,11 +5,11 @@ from pydub import AudioSegment
 import sounddevice as sd
 import sys
 
-CHUNK = 2048 
+CHUNK = 1024 * 2 
 
 song = AudioSegment.from_file("Moby Dick.wav")
 
-song = song.set_frame_rate(44100)
+#song = song.set_frame_rate(44100)
 
 RATE = song.frame_rate
 
@@ -29,10 +29,10 @@ def draw_spectrum(fft_array):
     heights = []
     max_fft = np.max(np.abs(fft_array))
     
-    max_index = int(20000 * (CHUNK / RATE))   
+    max_index = int(22050 * (CHUNK / RATE))   
 
     for i in range(1, min(max_index, CHUNK)): 
-        height = np.abs(fft_array[i]) / max_fft * MAX_HEIGHT  # scale to MAX_HEIGHT
+        height = np.abs(fft_array[i]) / max_fft * MAX_HEIGHT 
         x = int(np.log(i) / np.log(max_index) * WIDTH) 
         pygame.draw.line(screen, (0, 255, 0), (x, HEIGHT), (x, HEIGHT - height))
         heights.append((x, HEIGHT - height))
@@ -53,7 +53,7 @@ def draw_spectrum(fft_array):
 
 # precompute FFT for each chunk
 fft_arrays = []
-window = np.hanning(CHUNK)  # create a window
+window = np.kaiser(CHUNK, 14)  # create a window
 for i in range(len(song_array) // CHUNK):
     data = song_array[i*CHUNK:(i+1)*CHUNK]
     if song.channels == 2:
@@ -63,7 +63,114 @@ for i in range(len(song_array) // CHUNK):
     else:
         fft_array = fft(data * window)  
     fft_arrays.append(fft_array)
+####################################################
+fft_hanning = []
+window = np.hanning(CHUNK)  # create a window
+for i in range(len(song_array) // CHUNK):
+    data = song_array[i*CHUNK:(i+1)*CHUNK]
+    if song.channels == 2:
+        yf1 = fft(data[:, 0] * window)  
+        yf2 = fft(data[:, 1] * window)  
+        fft_array = (yf1 + yf2) / 2
+    else:
+        fft_array = fft(data * window)  
+    fft_hanning.append(fft_array)
+####################################################
+fft_hamming = []
+window = np.hamming(CHUNK)  # create a window
+for i in range(len(song_array) // CHUNK):
+    data = song_array[i*CHUNK:(i+1)*CHUNK]
+    if song.channels == 2:
+        yf1 = fft(data[:, 0] * window)  
+        yf2 = fft(data[:, 1] * window)  
+        fft_array = (yf1 + yf2) / 2
+    else:
+        fft_array = fft(data * window)  
+    fft_hamming.append(fft_array)
+####################################################
+fft_blackman = []
+window = np.blackman(CHUNK)  # create a window
+for i in range(len(song_array) // CHUNK):
+    data = song_array[i*CHUNK:(i+1)*CHUNK]
+    if song.channels == 2:
+        yf1 = fft(data[:, 0] * window)  
+        yf2 = fft(data[:, 1] * window)  
+        fft_array = (yf1 + yf2) / 2
+    else:
+        fft_array = fft(data * window)  
+    fft_blackman.append(fft_array)
+####################################################
+fft_bartlett = []
+window = np.bartlett(CHUNK)  # create a window
+for i in range(len(song_array) // CHUNK):
+    data = song_array[i*CHUNK:(i+1)*CHUNK]
+    if song.channels == 2:
+        yf1 = fft(data[:, 0] * window)  
+        yf2 = fft(data[:, 1] * window)  
+        fft_array = (yf1 + yf2) / 2
+    else:
+        fft_array = fft(data * window)  
+    fft_bartlett.append(fft_array)
+####################################################
+def use_kaiser():
+    global current_fft
+    global current_window
+    current_fft = fft_arrays
+    current_window = 'Kaiser'
 
+def use_hanning():
+    global current_fft
+    global current_window
+    current_fft = fft_hanning
+    current_window = 'Hanning'
+
+def use_hamming():
+    global current_fft
+    global current_window
+    current_fft = fft_hamming
+    current_window = 'Hamming'
+
+def use_bartlett():
+    global current_fft
+    global current_window
+    current_fft = fft_bartlett
+    current_window = 'Bartlett'
+
+def use_blackman():
+    global current_fft
+    global current_window
+    current_fft = fft_blackman
+    current_window = 'Blackman'
+
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, (0,0,0))
+    return textSurface, textSurface.get_rect()
+
+def draw_button(screen, message, x, y, w, h, ic, ac, action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(screen, ac,(x,y,w,h))
+        if click[0] == 1 and action != None:
+            action() 
+    else:
+        pygame.draw.rect(screen, ic,(x,y,w,h))
+
+    smallText = pygame.font.SysFont(None,20)
+    textSurf, textRect = text_objects(message, smallText)
+    textRect.center = ( (x+(w/2)), (y+(h/2)) )
+    screen.blit(textSurf, textRect)
+
+def draw_text_box(screen, message, x, y, w, h, color):
+    pygame.draw.rect(screen, color, (x, y, w, h))
+    smallText = pygame.font.SysFont(None, 20)
+    textSurf, textRect = text_objects(message, smallText)
+    textRect.center = ((x + (w / 2)), (y + (h / 2)))
+    screen.blit(textSurf, textRect)
+
+current_window = 'Kaiser'
+current_fft = fft_arrays
 # play the song and update the spectrum analyzer
 sd.play(song_array / np.max(np.abs(song_array)), RATE)
 pygame.time.set_timer(pygame.USEREVENT, int(1000 * CHUNK / RATE))  
@@ -78,7 +185,7 @@ while True:
         if event.type == pygame.USEREVENT:
             font = pygame.font.Font(None, 24) 
 
-            draw_spectrum(fft_arrays[fft_index])
+            draw_spectrum(current_fft[fft_index])
             pygame.draw.rect(screen, (200, 200, 200), slider_rect)  
             pygame.draw.circle(screen, (255, 0, 0), (slider_rect.left + int(slider_pos * slider_rect.width), slider_rect.centery), 20) 
             chunk_label = font.render("Zoom: " + str(CHUNK), 1, (255, 255, 255))
@@ -98,5 +205,12 @@ while True:
             slider_pos = max(0, min(1, slider_pos))  
 
             CHUNK = max(7, int(slider_pos * 1024))
-            
+    draw_button(screen, 'Kaiser', 50 + 300, 50, 100, 50, (0,200,0), (0,255,0), use_kaiser)
+    draw_button(screen, 'Hanning', 200 + 300, 50, 100, 50, (0,200,0), (0,255,0), use_hanning)    
+    draw_button(screen, 'Hamming', 350 + 300, 50, 100, 50, (0,200,0), (0,255,0), use_hamming)  
+    draw_button(screen, 'Bartlett', 500 + 300, 50, 100, 50, (0,200,0), (0,255,0), use_bartlett)
+    draw_button(screen, 'Blackman', 650 + 300, 50, 100, 50, (0,200,0), (0,255,0), use_blackman)
+    draw_text_box(screen, 'Current window: ' + current_window, 1200, 50, 200, 50, (200, 200, 200))
+
+    pygame.display.flip()
 
